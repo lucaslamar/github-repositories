3e2429014bbdfc16ee504ec93b420e96b428d964
Lucas Lamar <lucass.lamar@gmail.com>
2019-11-28 00:09:34 -0300

    lista repositorios

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -42,6 +42,28 @@ const rotate = keyframes`
   }
 `;
 
+export const List = styled.ul`
+  list-style: none;
+  margin-top: 30px;
+
+  li {
+    padding: 15px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    & + li {
+      border-top: 1px solid #eee;
+    }
+
+    a {
+      color: #7159c1;
+      text-decoration: none;
+    }
+  }
+`;
+
 export const SubmitButton = styled.button.attrs(props => ({
   type: 'submit',
   disabled: props.loding,
